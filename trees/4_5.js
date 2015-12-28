52ed6ebf36ce5bf34226fe5d0bde581ca397c7dc
Gaye Bulut <gayegul@gmail.com>
2015-12-28 10:12:55 -0800

    add solution

---
+++ trees/4_5.js
@@ -0,0 +1,11 @@
+//O(n)
+function validateBST(root) {
+  var leftResult = true;
+  var rightResult = true;
+  if(!root) return true;
+  if(root.left && root.left.val > root.val) return false;
+  else if(root.right && root.right.val < root.val) return false;
+  if(!validateBST(root.left)) return false;
+  else if(!validateBST(root.right)) return false;
+  return true;
+}
