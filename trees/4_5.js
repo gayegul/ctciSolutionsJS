2a59f85cd571faf95699eb744562cbf28eec4558
Gaye Bulut <gayegul@gmail.com>
2015-12-29 09:41:55 -0800

    minor fix

--- trees/4_5.js
+++ trees/4_5.js
@@ -3,8 +3,9 @@ function validateBST(root) {
   var leftResult = true;
   var rightResult = true;
   if(!root) return true;
-  if(root.left && root.left.val > root.val) return false;
+  else if(root.left && root.left.val > root.val) return false;
   else if(root.right && root.right.val < root.val) return false;
+
   if(!validateBST(root.left)) return false;
   else if(!validateBST(root.right)) return false;
   return true;
