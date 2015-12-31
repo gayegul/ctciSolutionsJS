b082f705d42021390dc5cd16732f87f9fa7d99b6
Gaye Bulut <gayegul@gmail.com>
2015-12-31 12:19:41 -0800

    add solution

---
+++ arraysStrings/1_1.js
@@ -0,0 +1,19 @@
+//With a hashtable O(n)
+function isUnique(str) {
+  if(!str) return null;
+  var seen = {};
+  for(var i = 0; i < str.length; i++) {
+    if(!seen[str[i]]) seen[str[i]] = true;
+    else return false;
+  }
+  return true;
+}
+
+//Without a hashtable 
+function isUnique(str) {
+  str = str.split('').sort();
+  for(var i = 0; i < str.length; i++) {
+    if(str[i] === str[i+1]) return false;
+  }
+  return true;
+}
