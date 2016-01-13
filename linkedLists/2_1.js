3cccf48d4e4a169221484e64457c4b6dee626fba
Gaye Bulut <gayegul@gmail.com>
2016-01-13 14:24:26 -0800

    add solution

--- linkedLists/2_1.js
+++ linkedLists/2_1.js
@@ -1,54 +1,36 @@
-//singly-linked list
-
-//invokes removeDuplicate (and removeDuplicateNoHash) methods
-LinkedList.prototype.findResult = function() {
-  if(!this.length) return null;
-  else if(this.length === 1) return this;
-  this.removeDuplicate();
-  return this;
-};
-
-//removes duplicates using a hashtable 0(n)
-LinkedList.prototype.removeDuplicate = function() {
-  var nodes = {};
-  var current = this.head;
+//With a hashtable
+LinkedList.prototype.removeDuplicates = function() {
+  if(!this.head || !this.head.next) return this;
+  var seen = {};
+  var current = this.head.next;
   var prev = this.head;
-  while(current.next) {
-    if(!nodes[current.data]) {
-      nodes[current.data] = true;
-    } else {
-      this.length--;
-      prev.next = current.next;
+  seen[prev.data] = true;
+  while(current) {
+    if(!seen[current.data]) {
+      seen[current.data] = true;
+      prev = prev.next;
     }
-    prev = current;
+    else prev.next = current.next;
     current = current.next;
   }
-  if(nodes[current.data]) {
-    prev.next = null;
-    this.length--;
-  }
   return this;
 };
 
-//removes duplicates without a hashtable 0(n2)
-LinkedList.prototype.removeDuplicateNoHash = function() {
-  var prev = this.head;
-  var length = this.length;
-  var current;
-  for(var i = 0; i < length - 1; i++) {
-    current = prev.next;
-    while(current.next) {
-      if(current.data === prev.data) {
-        prev.next = current.next;
-        this.length--;
+// //Without a hashtable (without sorting O(n2) time O(1) space)
+LinkedList.prototype.removeDuplicates = function() {
+  if(!this.head || !this.head.next) return this;
+  var current = this.head;
+  var runner = current;
+  while(current) {
+    while(runner.next) {
+      if(runner.next.data === current.data) {
+        runner.next = runner.next.next;
+      } else {
+        runner = runner.next;
       }
-      current = current.next;
     }
-    if(prev.data === current.data) {
-      prev.next = null;
-      this.length--;
-    }
-    prev = prev.next;
+    current = current.next;
+    runner = current;
   }
   return this;
 };
