35a1d0e3508fc38914857d9517455f84892e0181
Gaye Bulut <gayegul@gmail.com>
2016-01-14 09:43:38 -0800

    add solution

--- linkedLists/2_2.js
+++ linkedLists/2_2.js
@@ -1,33 +1,18 @@
-//finds kth element from tail in a singly-linked list given length and tail O(n)
-LinkedList.prototype.findKFromTail = function(number) {
+//finds kth element from tail in a
+//singly-linked list without length and tail O(n) time O(1) space
+LinkedList.prototype.findKFromTail = function(k) {
+  if(k < 0 || k > this.length) return null;
   var current = this.head;
-  var index = this.length - number;
-  if(index < 0 || number <= 0) return null;
-  else if(index === 0) return this.head;
-  else if(index === this.length) return this.tail;
-  else {
-    var counter = 0;
-    while(counter < index && current.next) {
-      current = current.next;
-      counter++;
-    }
-    return current;
-  }
-};
-
-//finds kth element from tail in a singly-linked list without length and tail O(n)
-LinkedList.prototype.findKFromTail = function(number) {
-  var fastPointer = this.head;
-  var slowPointer = this.head;
+  var fast = this.head;
   var counter = 0;
-  if(number < 0) return null;
-  while(fastPointer.next !== null) {
+  while(counter < k) {
+    fast = fast.next;
     counter++;
-    fastPointer = fastPointer.next;
-    if(fastPointer.next === null && counter < number - 1) return null;
-    else if(counter >= number) {
-      slowPointer = slowPointer.next;
-    }
   }
-  return slowPointer;
+  if(!fast) return null;
+  while(fast.next) {
+    current = current.next;
+    fast = fast.next;
+  }
+  return current;
 };
