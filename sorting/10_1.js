6f8f6ad9874d84c0bc944f83f914ed3034ed29ca
Gaye Bulut <gayegul@gmail.com>
2016-01-23 12:14:17 -0800

    add solution

---
+++ sorting/10_1.js
@@ -0,0 +1,16 @@
+function merge(arr1, arr2) {
+  var len = arr1.length + arr2.length;
+  var firstIndex = arr1.length - 1;
+  var secondIndex = arr2.length - 1;
+
+  for(var i = len - 1; i >= 0; i--) {
+    if(firstIndex >= 0 && secondIndex >= 0) {
+      arr1[i] = arr1[firstIndex] > arr2[secondIndex] ? arr1[firstIndex--] : arr2[secondIndex--];
+    } else if(firstIndex >= 0) {
+      arr1[i] = arr1[firstIndex--];
+    } else {
+      arr1[i] = arr2[secondIndex--];
+    }
+  }
+  return arr1;
+}
