c324fc4d473285d30908226ef5c0b70ac64658a6
Gaye Bulut <gayegul@gmail.com>
2016-01-26 18:13:20 -0800

    minor change

--- sorting/10_1.js
+++ sorting/10_1.js
@@ -6,11 +6,9 @@ function merge(arr1, arr2) {
   for(var i = len - 1; i >= 0; i--) {
     if(firstIndex >= 0 && secondIndex >= 0) {
       arr1[i] = arr1[firstIndex] > arr2[secondIndex] ? arr1[firstIndex--] : arr2[secondIndex--];
-    } else if(firstIndex >= 0) {
-      arr1[i] = arr1[firstIndex--];
-    } else {
-      arr1[i] = arr2[secondIndex--];
     }
+    else if(firstIndex >= 0) arr1[i] = arr1[firstIndex--];
+    else arr1[i] = arr2[secondIndex--];
   }
   return arr1;
 }
