72cc3a391dbabe58e8815b972bd3431838df44c0
Gaye Bulut <gayegul@gmail.com>
2016-01-30 11:56:02 -0800

    debug and refactor

--- trees/4_1.js
+++ trees/4_1.js
@@ -1,36 +1,21 @@
-//Depth first search
-Graph.prototype.isReachable = function(vertex, toVertex, seen) {
-  seen = seen || {};
-  var result = false;
-  for(var i = 0; i < vertex.from.length; i++) {
-    if(vertex.from[i] === toVertex) return true;
-    else if(!seen[vertex.from[i]]) {
-      seen[vertex.from[i]] = true;
-      result = this.isReachable(vertex.from[i], toVertex, seen);
-      if(result) return true;
-    }
-  }
-  return result;
-};
-
-//Breadth first search
+//Breadth First Search
 Graph.prototype.isReachable = function(vertex, toVertex) {
-  var seen = {};
-  var queue = [];
-  var item;
-  if(vertex.from.length === 0 || vertex === toVertex) return false;
-  while(true) {
-    for(var i = 0; i < vertex.from.length; i++) {
-      queue.push(vertex.from[i]);
+  var queue = [vertex];
+  var currentVertex;
+
+  //clear vertex.seen property from the previous invocation for all
+  for(var i = 0; i < this.vertexList.length; i++) {
+    this.vertexList[i].seen = false;
+  }
+
+  while(queue.length) {
+    currentVertex = queue.shift();
+    if(currentVertex === toVertex) return true;
+    else if(currentVertex.seen === true) continue;
+    else if(!currentVertex.seen) vertex.seen = true;
+    for(var j = 0; j < currentVertex.from.length; j++) {
+      queue.push(currentVertex.from[j]);
     }
-    item = queue.shift();
-    if(item.name === toVertex.name) return true;
-    else if(seen[item.name]) return false;
-    else if(!seen[item.name]) seen[item.name] = true;
-    vertex = item;
   }
   return false;
 };
-
-//TODO
-//Breadth first without a hashtable, using colors or such
