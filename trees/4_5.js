8a8e4dab91fab8dd2c9e842fd0ee2bb6e3031f59
Gaye Bulut <gayegul@gmail.com>
2016-02-06 18:31:36 -0800

    works now

--- trees/4_5.js
+++ trees/4_5.js
@@ -1,12 +1,20 @@
-//O(n)
-function validateBST(root) {
-  var leftResult = true;
-  var rightResult = true;
-  if(!root) return true;
-  else if(root.left && root.left.val > root.val) return false;
-  else if(root.right && root.right.val < root.val) return false;
+function checkBST(root, max, min) {
+  if(!root) return;
 
-  if(!validateBST(root.left)) return false;
-  else if(!validateBST(root.right)) return false;
-  return true;
+  if(root.left) {
+    if(root.left.data > root.data || root.left.data > max) return false;
+  }
+  if(root.right) {
+    if(root.right.data <= root.data || root.right.data <= min) return false;
+  }
+
+  var leftMin = min === 0 ? 0 : (min || -Infinity);
+  var leftMax = root.data;
+  var rightMin = root.data;
+  var rightMax = max === 0 ? 0 : (max || Infinity);
+
+  var left = checkBST(root.left, leftMax, leftMin);
+  var right = checkBST(root.right, rightMax, rightMin);
+  if(left === false || right === false) return false;
+  else return true;
 }
