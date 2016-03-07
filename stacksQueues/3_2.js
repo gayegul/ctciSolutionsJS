7fa1322bc1c7fc514598ac745caf81253e1f623c
Gaye Bulut <gayegul@gmail.com>
2016-03-07 13:38:20 -0800

    clean

--- stacksQueues/3_2.js
+++ stacksQueues/3_2.js
@@ -1,35 +1,69 @@
-var Stack = function() {
-  this.top = null;
-  this.min = [];
+//Solution with a queue implementation using Linked Lists
+var LinkedList = function() {
+  this.head = null;
+  this.tail = null;
+  this.length = 0;
 };
 
-var Node = function(val) {
-  this.val = val;
+var Node = function(data) {
+  this.data = data;
   this.next = null;
 };
 
-Stack.prototype.minStack = function() {
-  return this.min[this.min.length - 1];
+LinkedList.prototype.append = function(data) {
+  if(data === undefined) return null;
+  this.length++;
+  var node = new Node(data);
+  if(!this.head) this.head = node;
+  else this.tail.next = node;
+  this.tail = node;
+  return this.length;
 };
 
-Stack.prototype.push = function(item) {
-  if(!item) return null;
-  var node = new Node(item);
-  if(this.top) node.next = this.top;
-  this.top = node;
-  if(!this.min.length || item <= this.min[this.min.length - 1]) this.min.push(item);
+LinkedList.prototype.pop = function() {
+  if(!this.tail) return null;
+  this.length--;
+  var current = this.head;
+  if(this.tail === this.head) {
+    var data = this.tail.data;
+    this.head = null;
+    this.tail = null;
+    return data;
+  } else {
+    var lastBeforeTail = current;
+    while(current.next) {
+      lastBeforeTail = current;
+      current = current.next;
+    }
+    lastBeforeTail.next = null;
+    this.tail = lastBeforeTail;
+  }
+  return current.data;
+};
+
+var Stack = function() {
+  this.min = new LinkedList();
+  this.stack = new LinkedList();
+};
+
+Stack.prototype.push = function(data) {
+  if(!data) return null;
+  if(!this.stack.length) this.min.append(data);
+  else {
+    if(data < this.min.tail.data) this.min.append(data);
+  }
+  this.stack.append(data);
 };
 
 Stack.prototype.pop = function() {
-  var currentTop = this.top;
-  this.top = this.top.next;
-  currentTop.next = null;
-  if(this.min[this.min.length - 1] === currentTop.val) this.min.pop();
-  return currentTop;
+  if(!this.stack.length) return null;
+  else {
+    var popped = this.stack.pop();
+    if(popped === this.min.tail.data) this.min.pop();
+  }
 };
 
-var stack = new Stack();
-stack.push(4);
-stack.push(3);
-stack.push(2);
-stack.push(1);
+Stack.prototype.minimum = function() {
+  if(!this.min.length) return null;
+  return this.min.tail.data;
+};
