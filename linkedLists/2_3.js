43b6f69dc5ffc2c9f9346d76cb9273ba9fd47252
Gaye Bulut <gayegul@gmail.com>
2016-05-05 00:13:08 -0700

    update solution

--- linkedLists/2_3.js
+++ linkedLists/2_3.js
@@ -1,6 +1,7 @@
+// Copy the next node into the target node and delete the next one
 function deleteMiddleNode(node) {
   if(!node || !node.next) return false;
-  node.val = node.next.val;
+  node.val = node.next.data;
   node.next = node.next.next;
   return true;
 }
