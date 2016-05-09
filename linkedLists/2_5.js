2025068c5956c535d0ce4dd4802e89e7498981f4
Gaye Bulut <gayegul@gmail.com>
2016-05-08 19:25:24 -0700

    update solution

--- linkedLists/2_5.js
+++ linkedLists/2_5.js
@@ -1,46 +1,72 @@
-//Reverse order
-function sumLists(list1, list2) {
-  var num1 = '';
-  var num2 = '';
-  var current1 = list1.head;
-  while(current1) {
-    num1 = current1.data + num1;
-    current1 = current1.next;
+// Reverse order
+function addLists(head1, head2) {
+  if(!head1 && !head2) return null;
+  var length1 = getLength(head1);
+  var length2 = getLength(head2);
+  if(length1 < length2) {
+    head1 = padList(head1, length2 - length1);
+  } else {
+    head2 = padList(head2, length1 - length2);
   }
-  var current2 = list2.head;
-  while(current2) {
-    num2 = current2.data + num2;
-    current2 = current2.next;
-  }
-  var list = new LinkedList();
-  var result = Number(num1) + Number(num2);
-  while(result > 0) {
-    list.append(result % 10);
-    result = Math.floor(result / 10);
-  }
-  return list;
+  return sum(head1, head2);
 }
 
-//Forward order
-function sumLists(list1, list2) {
-  var num1 = '';
-  var num2 = '';
-  var current1 = list1.head;
+function sum(head1, head2) {
+  var leftover = 0;
+  var digit = 0;
+  var head = null;
+  var current1 = head1;
+  var current2 = head2;
   while(current1) {
-    num1 += current1.data;
+    digit = current1.data + current2.data;
+    if(leftover) digit += leftover;
+    if(digit > 9) leftover = 1;
+    var node = new Node(digit % 10);
+    if(!head) {
+      head = node;
+      tail = node;
+    }
+    else {
+      tail.next = node;
+      tail = node;
+    }
     current1 = current1.next;
-  }
-  var current2 = list2.head;
-  while(current2) {
-    num2 += current2.data;
     current2 = current2.next;
   }
-  var list = new LinkedList();
-  var result = Number(num1) + Number(num2);
-  console.log(result);
-  while(result > 0) {
-    list.append(result % 10);
-    result = Math.floor(result / 10);
+  if(leftover) {
+    var lastNode = new Node(leftover);
+    tail.next = lastNode;
   }
-  return list;
+  return head;
 }
+
+function getLength(head) {
+  if(!head) return 0;
+  var length = 1;
+  var current = head;
+  while(current.next) {
+    length++;
+    current = current.next;
+  }
+  return length;
+}
+
+function padList(head, k) {
+  if(!k || k < 0) return head;
+  var tail = head;
+  while(tail.next) {
+    tail = tail.next;
+  }
+  while(k) {
+    var node = new Node(0);
+    tail.next = node;
+    tail = node;
+    k--;
+  }
+  return head;
+}
+
+var Node = function(data) {
+  this.data = data;
+  this.next = null;
+};
