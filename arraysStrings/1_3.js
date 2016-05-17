1974bff2e324eedea1e95c3180c5aeaedbf27af3
Gaye Bulut <gayegul@gmail.com>
2016-05-17 10:35:32 -0700

    update solution

--- arraysStrings/1_3.js
+++ arraysStrings/1_3.js
@@ -1,8 +1,9 @@
 //O(n)
 function replaceSpace(str) {
+  if(!str) return;
   var arr = str.split('');
   for(var i = 0; i < arr.length; i++) {
-    if(arr[i].charAt(0) === ' ') arr[i] = '%20';
+    if(arr[i] === ' ') arr[i] = '%20';
   }
   return arr.join('');
 }
