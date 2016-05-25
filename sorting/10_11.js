e229ab15365a130322621ec121ef90dbfdcb0ed6
Gaye Bulut <gayegul@gmail.com>
2016-05-24 20:23:00 -0700

    update solution

--- sorting/10_11.js
+++ sorting/10_11.js
@@ -1,17 +1,24 @@
 function fixPeakValley(nums) {
   if(!nums || nums.length < 2) return nums;
-
   var peak = 0;
-  var valley = 1;
-
-  while(peak < nums.length - 1 && valley < nums.length - 1) {
-    if(nums[peak] < nums[valley]) {
-      var temp = nums[valley];
-      nums[valley] = nums[peak];
-      nums[peak] = temp;
-      valley += 2;
-    } else peak += 2;
+  for(var i = 0; i < nums.length - 1; i++) {
+    if(peak % 2 === 0) {
+      if(nums[i] < nums[i + 1]) {
+        var temp = nums[i + 1];
+        nums[i + 1] = nums[i];
+        nums[i] = temp;
+      }
+    } else {
+      if(nums[i] > nums[i + 1]) {
+        var temp2 = nums[i + 1];
+        nums[i + 1] = nums[i];
+        nums[i] = temp2;
+      }
+    }
+    peak++;
   }
-
   return nums;
 }
+
+fixPeakValley([9,1,0,4,8,7,10,0,11,11,3,4,5,6,7,0,2,2,2,12,10,7]);
+// [9, 0, 4, 1, 8, 7, 10, 0, 11, 3, 11, 4, 6, 5, 7, 0, 2, 2, 12, 2, 10, 7]
